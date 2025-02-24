23bcb36003f180e8449061410b9fba4bb58613b6
Hinca1100 <JHINCA6@bancodebogota.com.co>
2025-02-24 10:15:08 -0500

    update changes

--- src/services/reservation.service.ts
+++ src/services/reservation.service.ts
@@ -4,26 +4,62 @@ import { Room } from "../models/room.model";
 import { sendEmail } from "./send-email.service";
 
 export class ReservationService {
-  async createReservation(movieId: string, roomId: string, schedule: Date, seats: number[]) {
-    // Verifica que la película y la sala existen
+  constructor(private emailService = sendEmail) {} // Inyectamos sendEmail
+
+  async createReservation(
+    movieId: string,
+    roomId: string,
+    schedule: Date,
+    seats: number[]
+  ) {
+    const movie = await this.findMovieById(movieId);
+    const room = await this.findRoomById(roomId);
+    await this.validateSeatsAvailability(roomId, schedule, seats);
+
+    // Crea la reserva
+    const reservation = new Reservation({ movieId, roomId, schedule, seats });
+    const savedReservation = await reservation.save();
+
+    // Enviar correo de confirmación
+    await this.sendConfirmationEmail(savedReservation, movie, room);
+
+    return savedReservation;
+  }
+
+  private async findMovieById(movieId: string) {
     const movie = await Movie.findById(movieId);
     if (!movie) throw new Error("Película no encontrada");
+    return movie;
+  }
 
+  private async findRoomById(roomId: string) {
     const room = await Room.findById(roomId);
     if (!room) throw new Error("Sala no encontrada");
+    return room;
+  }
 
-    // Verifica que los asientos estén disponibles en la fecha y hora específica
-    const existingReservations = await Reservation.find({ roomId, schedule, seats: { $in: seats } });
-    if (existingReservations.length > 0) throw new Error("Asientos ocupados en este horario");
+  private async validateSeatsAvailability(
+    roomId: string,
+    schedule: Date,
+    seats: number[]
+  ) {
+    const existingReservations = await Reservation.find({
+      roomId,
+      schedule,
+      seats: { $in: seats },
+    });
+    if (existingReservations.length > 0)
+      throw new Error("Asientos ocupados en este horario");
+  }
 
-    // Crea la reserva incluyendo el horario
-    const reservation = new Reservation({ movieId, roomId, schedule, seats });
-    const response = await reservation.save();
-    if (response.movieId) {
-      const movie = await Movie.findById(response.movieId);
-      const room =  await Room.findById(response.roomId);
-    await sendEmail("hinca100@hotmail.com",movie?.title, room?.name,response.seats,response.schedule);
-    }
-    return response;
+  private async sendConfirmationEmail(reservation: any, movie: any, room: any) {
+    if (!movie || !room) return;
+    await this.emailService(
+      "hinca100@hotmail.com",
+      movie.title,
+      room.name,
+      reservation.seats,
+      reservation.schedule
+    );
   }
 }
